fe66aa167ed7fff1d3ce403b612d8db90b1e5edd
Dominic Chu <dominic.m.chu@gmail.com>
2019-09-27 06:30:06 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,12 +3,23 @@ def random_number_generator
 end
 
 def user_input_prompt
-  puts ""
+  puts "Please guess a number between 1 and 6"
+end
+
+def gets_user_input
+  gets.chomp
 end
 
 
 def run_guessing_game
   computer_number = random_number_generator
   user_input_prompt
-
+  user_guessed = gets_user_input
+  if user_guessed == computer_number
+    puts "You guessed the correct number!"
+  elsif user_guessed == "exit"
+    puts "Goodbye!"
+  else
+    puts "Sorry! The computer guessed #{computer_number}."
+  
 end
