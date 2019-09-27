3ad6548f2ea13d3c060e525483f06c4cedb3e3d1
Dominic Chu <dominic.m.chu@gmail.com>
2019-09-27 06:26:51 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,19 @@
-# Code your solution here!
+def random_number_generator 
+  rand(6) + 1
+end
+
+def user_input_prompt
+  puts ""
+end
+
+
+def run_guessing_game 
+  computer_number = random_number_generator
+  user_input_prompt
+
+end
+
+
+
+
+
